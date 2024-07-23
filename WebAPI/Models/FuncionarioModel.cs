bc3906b257eba92990c0be84a2461e09e381b73b
jfsdias <jfsdias@FUNDAT-CETI06>
2024-07-23 14:50:27 -0300

    Adicionar arquivos de projeto.

---
+++ WebAPI/Models/FuncionarioModel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Specialized;
+using System.ComponentModel.DataAnnotations;
+using WebAPI.Enums;
+
+namespace WebAPI.Models
+{
+    public class FuncionarioModel
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Sobrenome { get; set; }
+        public DepartamentoEnum Departamento { get; set; }
+        public bool Ativo {  get; set; }
+        public TurnoEnum Turno { get; set; }
+        public DateTime DataDeCriacao { get; set; } = DateTime.Now.ToLocalTime();
+        public DateTime DataDeAlteracao { get; set; } = DateTime.Now.ToLocalTime();
+
+    }
+}
